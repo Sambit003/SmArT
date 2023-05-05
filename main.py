5e3e0075de79f01aadba5fddf20e96e9f9d9c152
Sayandeep <sayandeep11@gmail.com>
2023-05-05 17:16:42 +0530

    Added: Pickle Integration for saving data in Pickle format

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import os
+import os.path
 import datetime
-import subprocess
+import pickle
+
 import tkinter as tk
 import cv2
-import util
 from PIL import Image, ImageTk
+import face_recognition
+
+import util
 
 
 class App:
@@ -12,10 +15,10 @@ class App:
         self.main_window = tk.Tk()
         self.main_window.geometry("1200x520+350+100")
 
-        self.login_button_main_window = util.get_button(self.main_window, 'Validate', 'green', self.login, fg='black')
+        self.login_button_main_window = util.get_button(self.main_window, 'Validate', 'blue', self.login)
         self.login_button_main_window.place(x=750, y=300)
 
-        self.register_new_user_button_main_window = util.get_button(self.main_window, 'Register New User', 'gray',
+        self.register_new_user_button_main_window = util.get_button(self.main_window, 'register new user', 'gray',
                                                                     self.register_new_user, fg='black')
         self.register_new_user_button_main_window.place(x=750, y=400)
 
@@ -23,29 +26,27 @@ class App:
         self.webcam_label.place(x=10, y=0, width=700, height=500)
 
         self.add_webcam(self.webcam_label)
+
         self.db_dir = './db'
         if not os.path.exists(self.db_dir):
             os.mkdir(self.db_dir)
 
-        self.log_path='./attendance_log.txt'
+        self.log_path = './attendance_log.txt'
 
     def add_webcam(self, label):
         if 'cap' not in self.__dict__:
             self.cap = cv2.VideoCapture(0)
 
         self._label = label
-
         self.process_webcam()
 
     def process_webcam(self):
         ret, frame = self.cap.read()
-        self.most_recent_capture_arr = frame
 
+        self.most_recent_capture_arr = frame
         img_ = cv2.cvtColor(self.most_recent_capture_arr, cv2.COLOR_BGR2RGB)
         self.most_recent_capture_pil = Image.fromarray(img_)
-
         imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
-
         self._label.imgtk = imgtk
         self._label.configure(image=imgtk)
 
@@ -53,33 +54,24 @@ class App:
 
     def login(self):
 
+        name = util.recognize(self.most_recent_capture_arr, self.db_dir)
 
-        unknown_img_path = './.tmp.jpg'
-
-        cv2.imwrite(unknown_img_path, self.most_recent_capture_arr)
-
-        output = str(subprocess.check_output(['face_recognition', self.db_dir, unknown_img_path]))
-        name= output.split(',')[1][:-3]
         if name in ['unknown_person', 'no_persons_found']:
-            util.msg_box('Oops....', 'Unknown Student, Please register new student or try again!')
+            util.msg_box('Sorry!', 'No Student Found, Please Try Again!')
         else:
-            util.msg_box('Welcome Back!', 'Welcome, {}'.format(name))
+            util.msg_box('Welcome back !', 'Welcome Back, \nHave a Nice Day !, {}.'.format(name))
             with open(self.log_path, 'a') as f:
-                f.write('{},\t{}\n'.format(name, datetime.datetime.now()))
+                f.write('{}\t{}\n'.format(name, datetime.datetime.now()))
                 f.close()
-        os.remove(unknown_img_path)
 
     def register_new_user(self):
         self.register_new_user_window = tk.Toplevel(self.main_window)
         self.register_new_user_window.geometry("1200x520+370+120")
 
-        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Accept', 'green',
-                                                                      self.accept_register_new_user, fg='black')
+        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Register', 'green', self.accept_register_new_user)
         self.accept_button_register_new_user_window.place(x=750, y=300)
 
-        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Try Again',
-                                                                         'red', self.try_again_register_new_user,
-                                                                         fg='black')
+        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Try again', 'red', self.try_again_register_new_user)
         self.try_again_button_register_new_user_window.place(x=750, y=400)
 
         self.capture_label = util.get_img_label(self.register_new_user_window)
@@ -90,8 +82,7 @@ class App:
         self.entry_text_register_new_user = util.get_entry_text(self.register_new_user_window)
         self.entry_text_register_new_user.place(x=750, y=150)
 
-        self.text_label_register_new_user = util.get_text_label(self.register_new_user_window,
-                                                                "Please, \nEnter the Student's Name: ")
+        self.text_label_register_new_user = util.get_text_label(self.register_new_user_window, 'Please, \nEnter Student Name:')
         self.text_label_register_new_user.place(x=750, y=70)
 
     def try_again_register_new_user(self):
@@ -110,9 +101,12 @@ class App:
     def accept_register_new_user(self):
         name = self.entry_text_register_new_user.get(1.0, "end-1c")
 
-        cv2.imwrite(os.path.join(self.db_dir, '{}.jpg'.format(name)), self.register_new_user_capture)
+        embeddings = face_recognition.face_encodings(self.register_new_user_capture)[0]
 
-        util.msg_box('Success!', 'Student Registered Successfully!')
+        file = open(os.path.join(self.db_dir, '{}.pickle'.format(name)), 'wb')
+        pickle.dump(embeddings, file)
+
+        util.msg_box('Success!', 'Student was registered successfully !')
 
         self.register_new_user_window.destroy()
 
